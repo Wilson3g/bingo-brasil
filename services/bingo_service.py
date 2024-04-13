758e6fc77a33174cc3944f8a3b551d62fb2cfd64
Wilson Junior <wilson.junior@omie.com.br>
2024-04-12 21:16:15 -0300

    feat: implementing repository pattern

--- services/bingo_service.py
+++ services/bingo_service.py
@@ -1,10 +1,13 @@
 import random
 from typing import List
 
+from repository.bingo_repository_in_memory import (
+    BingoRepositoryInterface
+)
+
 class BingoService:
-    def __init__(self):
-        self.__drawn_numbers = []
-        self.__cards_database_list = []
+    def __init__(self, respository):
+        self.__repository = respository if respository else BingoRepositoryInterface()
 
     def get_bingo(self):
         return {"Hello": "World"}
@@ -26,30 +29,30 @@ class BingoService:
             self.__get_numbers(61, 75)
         ]
         card_ordered_list = sum(card, [])
-        for card_database in self.__cards_database_list:
+        for card_database in self.__repository.get_all_cards():
             if card_database == card_ordered_list:
                 return self.get_card()
-        self.__cards_database_list.append(card_ordered_list)
+        self.__repository.insert_card(card_ordered_list)
 
         return card
     
     def get_drawn_numbers(self, start: int = 1, end: int = 75) -> int:
-        if len(self.__drawn_numbers) >= end:
+        if len(self.__repository.get_drawn_numbers()) >= end:
             return 0
         number = random.randint(start, end)
-        if number in self.__drawn_numbers:
+        if number in self.__repository.get_drawn_numbers():
             return self.get_drawn_numbers(start, end)
-        self.__drawn_numbers.append(number)
+        self.__repository.insert_drawn_number(number)
         return number
     
     def get_drawn_numbers_list_and_percentage(self, total_bingo_numbers: int = 75) -> tuple:
-        percentage = (len(self.__drawn_numbers)/total_bingo_numbers) * 100
-        return self.__drawn_numbers, round(percentage, 2)
+        percentage = (len(self.__repository.get_drawn_numbers())/total_bingo_numbers) * 100
+        return self.__repository.get_drawn_numbers(), round(percentage, 2)
 
     def has_bingo(self) -> bool:
         bingo = False
-        bingo_numbers = self.__drawn_numbers
-        for card in self.__cards_database_list:
+        bingo_numbers = self.__repository.get_drawn_numbers()
+        for card in self.__repository.get_all_cards():
             if set(card).issubset(set(bingo_numbers)):
                 return True
         return bingo
