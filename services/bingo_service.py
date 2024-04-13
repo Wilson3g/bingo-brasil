dcca4a2d4ae33a30df30489e0ab40d21392d5ca2
Wilson Junior <wilson.junior@omie.com.br>
2024-04-13 09:50:21 -0300

    refactor: adding typing

--- services/bingo_service.py
+++ services/bingo_service.py
@@ -2,18 +2,19 @@ import random
 from typing import List
 
 from repository.bingo_repository_in_memory import (
-    BingoRepositoryInterface
+    BingoRepositoryInterface,
+    BingoInMemoryReposository
 )
 
 class BingoService:
-    def __init__(self, respository):
+    def __init__(self, respository: BingoRepositoryInterface | BingoInMemoryReposository):
         self.__repository = respository if respository else BingoRepositoryInterface()
 
     def get_bingo(self):
         return {"Hello": "World"}
     
     def __get_numbers(self, start: int, end: int) -> List[int]:
-        numbers_list = []
+        numbers_list: list[int] = []
         while True:
             number = random.randint(start, end)
             if number not in numbers_list: numbers_list.append(number)
@@ -28,8 +29,8 @@ class BingoService:
             self.__get_numbers(46, 60),
             self.__get_numbers(61, 75)
         ]
-        card_ordered_list = sum(cards, [])
-        all_cards = self.__repository.get_all_cards()
+        card_ordered_list: list[int] = sum(cards, [])
+        all_cards: list[int] = self.__repository.get_all_cards()
         for card in all_cards:
             if card == card_ordered_list:
                 return self.get_card()
@@ -38,23 +39,23 @@ class BingoService:
         return cards
     
     def get_drawn_numbers(self, start: int = 1, end: int = 75) -> int:
-        drawn_numbers = self.__repository.get_drawn_numbers()
+        drawn_numbers: list[int] = self.__repository.get_drawn_numbers()
         if len(drawn_numbers) >= end:
             return 0
-        number = random.randint(start, end)
+        number: int = random.randint(start, end)
         if number in drawn_numbers:
             return self.get_drawn_numbers(start, end)
         self.__repository.insert_drawn_number(number)
         return number
     
     def get_drawn_numbers_list_and_percentage(self, total_bingo_numbers: int = 75) -> tuple:
-        drawn_numbers = self.__repository.get_drawn_numbers()
-        percentage = (len(drawn_numbers)/total_bingo_numbers) * 100
+        drawn_numbers: list[int] = self.__repository.get_drawn_numbers()
+        percentage: float = (len(drawn_numbers)/total_bingo_numbers) * 100
         return drawn_numbers, round(percentage, 2)
 
     def has_bingo(self) -> bool:
-        bingo = False
-        bingo_numbers = self.__repository.get_drawn_numbers()
+        bingo: bool = False
+        bingo_numbers: list[int] = self.__repository.get_drawn_numbers()
         cards = self.__repository.get_all_cards()
         for card in cards:
             if set(card).issubset(set(bingo_numbers)):
